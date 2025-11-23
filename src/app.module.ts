7a7a091c8caf960a4afc9a8d9d3ed5f54ada0f56
Victor Aponte <vaponte520@gmail.com>
2025-11-22 22:58:41 -0300

    first commmit

---
+++ src/app.module.ts
@@ -0,0 +1,35 @@
+// src/app.module.ts
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { validate } from './config/env.config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate,
+      envFilePath: '.env',
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres' as const,
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,                 // solo en desarrollo
+        logging: true,
+        // autoLoadEntities: true,         // si usas forFeature en otros módulos
+      }),
+      inject: [ConfigService],
+    }),
+
+    // Tus otros módulos irán aquí
+  ],
+})
+export class AppModule { }
