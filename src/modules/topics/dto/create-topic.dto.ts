1388997aac7ae03716a12f5e84451b71edf0d9f5
Victor Aponte <vaponte520@gmail.com>
2025-12-14 21:27:40 -0300

    feat(topics): Actualizar las rutas de importación para utilizar el alias @/ y obtener importaciones más limpias. Añadir archivos DTO y de entidad de tema.
    Implementar la creación y actualización de DTO junto con la entidad de tema para admitir la funcionalidad de gestión de temas.

---
+++ src/modules/topics/dto/create-topic.dto.ts
@@ -0,0 +1,15 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+
+export class CreateTopicDto {
+    @IsString()
+    @IsNotEmpty()
+    title: string;
+
+    @IsString()
+    @IsNotEmpty()
+    description: string;
+
+    @IsString()
+    @IsNotEmpty()
+    tags: string;
+}
